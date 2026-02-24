395665dec45185e83418c5ac9d536bff7305e843
Samuel <aleksandersamuel34937761@gmail.com>
2026-02-23 21:42:42 -0300

    Creating client class

--- complexBank.py
+++ complexBank.py
@@ -1,3 +1,15 @@
 #Complex bank system using opp(poo)
 
-print(8)
+from datetime import date #To define client age.
+
+class Client(): #Client class to associate an individual client to his related datas.
+    def __init__(self, cpf, name, email, birthday):
+        self.cpf = cpf
+        self.name = name
+        self.email = email
+        self.birthday = date
+
+
+
+
+
